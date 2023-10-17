e960ad15d0fd13bcfdbf8f9c274eefdff0cc626e
Kartikey Chaudhary <144946133+kartikey-09@users.noreply.github.com>
2023-10-17 19:59:31 +0530

    Add files via upload

---
+++ OOPs/classs.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class employee 
+{
+    public: 
+    int name;
+    int id;
+    private:
+    int salary;
+
+    employee()
+    {
+        
+    }
+};
+int main()
+{
+
+}
---
+++ OOPs/first.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "second.cpp"
+using namespace std;
+
+int main()
+{
+    kartikey k1;
+    cout<<"SIZE :"<<sizeof(k1);
+}
